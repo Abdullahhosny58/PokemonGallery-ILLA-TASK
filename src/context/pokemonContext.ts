da6b414a7842df3478de0d4323b4dad63d9c3006
Abdullahhosny58 <115153502+Abdullahhosny58@users.noreply.github.com>
2024-03-16 22:36:14 +0200

    update

--- src/context/pokemonContext.ts
+++ src/context/pokemonContext.ts
@@ -1,13 +1,17 @@
 import { createContext } from "react";
 import { Pokemon } from "../hooks/usePokemonData";
 
+// Define the types for the context
 interface PokmensContextTypes {
-  pokemonData: Array<Pokemon>;
-  handleSearchValue: (searchValue: string) => void;
+  pokemonData: Array<Pokemon>; // Array of Pokemon objects
+  handleSearchValue: (searchValue: string) => void; // Function to handle search value changes
 }
+
+// Initial value for the context
 const intialValue: PokmensContextTypes = {
-  pokemonData: [],
-  handleSearchValue: () => {},
+  pokemonData: [], // Initial empty array for pokemon data
+  handleSearchValue: () => {}, // Placeholder function for handling search value changes
 };
 
+// Create the context with initial value
 export const PokmensContext = createContext<PokmensContextTypes>(intialValue);
